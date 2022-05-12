2391e7d8552f5d5290c80ae200a09a768c4f7e9e
Natsuki469 <anto.zugey@gmail.com>
2022-05-12 13:18:29 -0400

    Cambie el título

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter With Git'),
+      home: const MyHomePage(title: 'Flutter Git'),
     );
   }
 }
