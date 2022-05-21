6dfdd872a087b421f9ac47616bf4110d6f36366f
Oscar Castro <alexcaxtro@gmail.com>
2022-05-20 21:54:43 -0400

    Se elimino el banner debug

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false, //quitar el banner debug
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.brown,
